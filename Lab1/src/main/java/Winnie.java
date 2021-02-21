f9e03b93f4832653f59c6d22829507d51056b794
Marina Montag <montag.marinka@gmail.com>
2021-02-21 14:20:43 +0200

    implement class Winnie

--- Lab1/src/main/java/Winnie.java
+++ Lab1/src/main/java/Winnie.java
@@ -1,2 +1,31 @@
-public class Winnie {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winnie implements Runnable{
+    private final List<Bee>bees;
+    private final Pot pot;
+    private final int iterations=5;
+    Winnie(List<Bee>bees,Pot pot){
+        this.bees=bees;
+        this.pot=pot;
+        new Thread(this).start();
+    }
+    @Override
+    public void run() {
+        for(int i=0;i<iterations;i++){
+            synchronized (this){
+                try {
+                    wait();
+                    pot.eatHoney();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
